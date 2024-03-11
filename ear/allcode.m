8ed9e810618be640f1fd7276af50b693dbde2180
Xiran Xu <48015859+xuxiran@users.noreply.github.com>
2024-03-11 17:45:23 +0800

    Update allcode.m

--- ear/allcode.m
+++ ear/allcode.m
@@ -1,20 +1,20 @@
-cd preprocess\
+cd preprocess/
 
     cuteeg
     disp('-------------cuteeg----------------')
-cd ..\
+cd ../
 
-cd analysis\
-    cd matlab\
+cd analysis/
+    cd matlab/
         cd code_space
             preprocess_IIR
             disp('-------------space----------------')
-        cd ..\
-        cd code_env\
+        cd ../
+        cd code_env/
             envelope_process_ear
             disp('-------------envelope----------------')
-        cd ..\
-    cd ..\
-cd ..\
+        cd ../
+    cd ../
+cd ../
 
 
